feff7f33d8a9733d3bca4e97eea95d080c3d9f94
Abubakar Chanda <abubakarchanda3@gmail.com>
2026-08-14 16:15:38 +0530

    design: increase blue lens halo brightness & expand background grid mask radius on homepage

--- src/components/site/hero.tsx
+++ src/components/site/hero.tsx
@@ -19,8 +19,8 @@ export function HomeHero() {
       className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-background pt-28 pb-16"
     >
       {/* Light background grid & ambient light radial gradient */}
-      <div className="pointer-events-none absolute inset-0 grid-bg opacity-70" />
-      <div className="pointer-events-none absolute top-1/3 left-1/2 h-[450px] w-[650px] -translate-x-1/2 -translate-y-1/2 halo blur-3xl opacity-50" />
+      <div className="pointer-events-none absolute inset-0 grid-bg opacity-90" />
+      <div className="pointer-events-none absolute top-1/3 left-1/2 h-[600px] w-[900px] -translate-x-1/2 -translate-y-1/2 halo blur-3xl opacity-85" />
 
       <div className="relative z-10 mx-auto max-w-5xl px-6 text-center">
         <Reveal>
